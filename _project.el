f69c7b103600de3c949a839ae12ef31e54b0a36a
Philip Dorrell <philip@Cangallo-Ubuntu.(none)>
2013-06-27 11:31:40 +1200

    add _project.el

---
+++ _project.el
@@ -0,0 +1,9 @@
+
+(load-this-project
+ `( (:ruby-executable ,*ruby-1.9-executable*)
+    (:run-project-command (ruby-run-file ,(concat (project-base-directory) "correspondence-ml.rb")))
+    (:build-function project-compile-with-command)
+    (:compile-command "rake")
+    (:run-project-command (rspec-file ,(concat (project-base-directory) "spec/test_spec.rb")))
+    (:ruby-args (,(concat "-I" (project-base-directory) "lib")))
+    ) )
