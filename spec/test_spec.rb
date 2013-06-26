4b2c46ec7c391420081d77f3a3ef3d9c578e184a
Philip Dorrell <philip@Cangallo-Ubuntu.(none)>
2013-06-27 11:57:55 +1200

    parse a number

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,8 +1,21 @@
 require 'correspondence-ml'
 
-describe "something" do
-  it "does something" do
-    CorrespondenceMarkupLanguage.sayHello.should == "hello"
+describe "markup language grammar" do
+  it "parses number with decimal digits only" do
+    parser = CorrespondenceMarkupLanguageParser.new
+    parser.parse("45").should_not be_nil
   end
+  
+  it "doesn't parses any string with non-decimal digits" do
+    parser = CorrespondenceMarkupLanguageParser.new
+    parser.parse("jim").should be_nil
+    parser.parse("-45").should be_nil
+    parser.parse("26e").should be_nil
+  end
+  
+  it "doesn't parse an empty string" do
+    parser = CorrespondenceMarkupLanguageParser.new
+    parser.parse("").should be_nil
+  end
+  
 end
-
