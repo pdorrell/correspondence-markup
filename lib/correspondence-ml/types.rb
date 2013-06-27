58181f062fc81e7cd58a5a8996bf3e3d61b325ee
Philip Dorrell <philip@Cangallo-Ubuntu.(none)>
2013-06-27 15:28:19 +1200

    Item attributes are id & text & define NonItem class

--- lib/correspondence-ml/types.rb
+++ lib/correspondence-ml/types.rb
@@ -1,15 +1,26 @@
 
 module CorrespondenceMarkup
+  
+  # An item is text in a structure with an associated id
   class Item
-    attr_reader :number, :text
+    attr_reader :id, :text
     
-    def initialize(number, text)
-      @number = number
+    def initialize(id, text)
+      @id = id
       @text = text
     end
     
     def ==(otherItem)
-      otherItem.class == Item && otherItem.number == @number && otherItem.text == @text
+      otherItem.class == Item && otherItem.id == @id && otherItem.text == @text
+    end
+  end
+  
+  # A non-item is text in a structure that is not an item - it is just text
+  class NonItem
+    attr_reader :text
+    
+    def initialize(text)
+      @text = text
     end
   end
 end
--- spec/types_spec.rb
+++ spec/types_spec.rb
@@ -3,6 +3,12 @@ require 'correspondence-ml/types'
 module CorrespondenceMarkup
   describe 'corresondence markup types' do
     
+    it "item has id and text attribute" do
+      item = Item.new(34, "text")
+      item.id.should == 34
+      item.text.should == "text"
+    end
+    
     it "items with same number and text are equal" do
       Item.new(34, "text").should == Item.new(34, "text")
       Item.new(34, "text").should_not == "something else"
