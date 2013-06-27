c3ac1d0992327b4e5615bef65e3f19ed4855199d
Philip Dorrell <philip@Cangallo-Ubuntu.(none)>
2013-06-27 12:22:11 +1200

    should_parse & should_not_parse methods

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -6,18 +6,25 @@ describe "markup language grammar" do
     @parser = CorrespondenceMarkupLanguageParser.new
   end
   
+  def parse_number(string)
+    @parser.parse(string, root: :number)
+  end
+  
+  def should_parse(root, strings)
+    for string in strings
+      @parser.parse(string, root: root).should_not be_nil
+    end
+  end
+  
+  def should_not_parse(root, strings)
+    for string in strings
+      @parser.parse(string, root: root).should be_nil
+    end
+  end
+  
   it "parses number with one or more decimal digits only" do
-    @parser.parse("45", root: :number).should_not be_nil
-  end
-  
-  it "doesn't parses any string with non-decimal digits" do
-    @parser.parse("jim", root: :number).should be_nil
-    @parser.parse("-45", root: :number).should be_nil
-    @parser.parse("26e", root: :number).should be_nil
-  end
-  
-  it "doesn't parse an empty string" do
-    @parser.parse("", root: :number).should be_nil
+    should_parse(:number, ["45", "5", "6677", "99999988"])
+    should_not_parse(:number, ["", "-45", "6 7", "jim"])
   end
   
 end
