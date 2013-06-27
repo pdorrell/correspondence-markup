105708c9939995e901462e74abf979a073c698e1
Philip Dorrell <philip@Cangallo-Ubuntu.(none)>
2013-06-27 15:22:02 +1200

    compile item

--- spec/compiler_spec.rb
+++ spec/compiler_spec.rb
@@ -1,17 +1,25 @@
 require 'correspondence-ml'
 
-describe "markup language compilation" do
-  
-  before(:all) do
-    @parser = CorrespondenceMarkupLanguageParser.new
+module CorrespondenceMarkup
+
+  describe "markup language compilation" do
+    
+    before(:all) do
+      @parser = CorrespondenceMarkupLanguageParser.new
+    end
+    
+    def parse(string, root = :structure_group)
+      @parser.parse(string, root: root)
+    end
+    
+    it "compiles number" do
+      parse("234", :number).value.should == 234
+    end
+    
+    it "compiles item" do
+      parse("[31 text]", :item).value.should == Item.new(31, "text")
+    end
+    
   end
-  
-  def parse(string, root = :structure_group)
-    @parser.parse(string, root: root)
-  end
-  
-  it "compiles number" do
-    parse("234", :number).value.should == 234
-  end
-  
+
 end
