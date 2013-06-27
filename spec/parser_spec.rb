387a14466d842e9c8c49886e400946aaf011401e
Philip Dorrell <philip@Cangallo-Ubuntu.(none)>
2013-06-27 12:16:43 +1200

    rspec define @parser in before hook

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,21 +1,23 @@
 require 'correspondence-ml'
 
 describe "markup language grammar" do
-  it "parses number with decimal digits only" do
-    parser = CorrespondenceMarkupLanguageParser.new
-    parser.parse("45", root: :number).should_not be_nil
+  
+  before(:all) do
+    @parser = CorrespondenceMarkupLanguageParser.new
+  end
+  
+  it "parses number with one or more decimal digits only" do
+    @parser.parse("45", root: :number).should_not be_nil
   end
   
   it "doesn't parses any string with non-decimal digits" do
-    parser = CorrespondenceMarkupLanguageParser.new
-    parser.parse("jim", root: :number).should be_nil
-    parser.parse("-45", root: :number).should be_nil
-    parser.parse("26e", root: :number).should be_nil
+    @parser.parse("jim", root: :number).should be_nil
+    @parser.parse("-45", root: :number).should be_nil
+    @parser.parse("26e", root: :number).should be_nil
   end
   
   it "doesn't parse an empty string" do
-    parser = CorrespondenceMarkupLanguageParser.new
-    parser.parse("", root: :number).should be_nil
+    @parser.parse("", root: :number).should be_nil
   end
   
 end
