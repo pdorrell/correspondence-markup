7375e93d60fd0aea68b5e5fe4ffb84c66cb76e96
Philip Dorrell <philip@Cangallo-Ubuntu.(none)>
2013-06-27 15:31:33 +1200

    test NonItem text attribute

--- spec/types_spec.rb
+++ spec/types_spec.rb
@@ -3,18 +3,28 @@ require 'correspondence-ml/types'
 module CorrespondenceMarkup
   describe 'corresondence markup types' do
     
-    it "item has id and text attribute" do
-      item = Item.new(34, "text")
-      item.id.should == 34
-      item.text.should == "text"
+    describe 'Item type' do
+      it "item has id and text attribute" do
+        item = Item.new(34, "text")
+        item.id.should == 34
+        item.text.should == "text"
+      end
+      
+      it "items with same number and text are equal" do
+        Item.new(34, "text").should == Item.new(34, "text")
+        Item.new(34, "text").should_not == "something else"
+        Item.new(34, "text").should_not == Item.new(35, "text")
+        Item.new(34, "text").should_not == Item.new(34, "different text")
+        Item.new(34, "text").should_not == Item.new(35, "different text")
+      end
     end
     
-    it "items with same number and text are equal" do
-      Item.new(34, "text").should == Item.new(34, "text")
-      Item.new(34, "text").should_not == "something else"
-      Item.new(34, "text").should_not == Item.new(35, "text")
-      Item.new(34, "text").should_not == Item.new(34, "different text")
-      Item.new(34, "text").should_not == Item.new(35, "different text")
+    describe 'NonItem type' do
+      it 'non-item has text attribute' do
+        nonItem = NonItem.new("the text")
+        nonItem.text.should == "the text"
+      end
     end
+    
   end
 end
