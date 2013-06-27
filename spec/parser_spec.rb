562bd834080b45233cb2ad7d7046a0ecfc568e39
Philip Dorrell <philip@Cangallo-Ubuntu.(none)>
2013-06-27 12:05:33 +1200

    rename to parser_spec, specify :root rule for number parser tests

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -3,19 +3,19 @@ require 'correspondence-ml'
 describe "markup language grammar" do
   it "parses number with decimal digits only" do
     parser = CorrespondenceMarkupLanguageParser.new
-    parser.parse("45").should_not be_nil
+    parser.parse("45", root: :number).should_not be_nil
   end
   
   it "doesn't parses any string with non-decimal digits" do
     parser = CorrespondenceMarkupLanguageParser.new
-    parser.parse("jim").should be_nil
-    parser.parse("-45").should be_nil
-    parser.parse("26e").should be_nil
+    parser.parse("jim", root: :number).should be_nil
+    parser.parse("-45", root: :number).should be_nil
+    parser.parse("26e", root: :number).should be_nil
   end
   
   it "doesn't parse an empty string" do
     parser = CorrespondenceMarkupLanguageParser.new
-    parser.parse("").should be_nil
+    parser.parse("", root: :number).should be_nil
   end
   
 end
