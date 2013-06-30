b1f073f2177485d153c95e8f87c42ca042977cf8
Philip Dorrell <philip@Cangallo-Ubuntu.(none)>
2013-06-30 14:31:24 +1200

    call binding in output_html param

--- spec/generate_html_spec.rb
+++ spec/generate_html_spec.rb
@@ -9,16 +9,15 @@ module CorrespondenceMarkup
       @parser = CorrespondenceMarkupLanguageParser.new
     end
     
-    def output_html(object_binding, template_text)
+    def output_html(template_binding, template_text)
       template = ERB.new(template_text, nil, nil)
-      template.result(object_binding)
+      template.result(template_binding)
     end
 
     it "outputs HTML for an item" do
       item_template = %{<span data-id="<%=item.id%>"><%=item.text%></span>}
       item = Item.new(21, "the text")
-      @binding = binding
-      output_html(@binding, item_template).should == %{<span data-id="21">the text</span>}
+      output_html(binding, item_template).should == %{<span data-id="21">the text</span>}
     end
   end
 end
