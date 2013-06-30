803e57ba77c4a417cca71d416f8be2aa35346c24
Philip Dorrell <philip@Cangallo-Ubuntu.(none)>
2013-06-30 14:44:05 +1200

    test output & template from files

--- spec/generate_html_spec.rb
+++ spec/generate_html_spec.rb
@@ -9,16 +9,25 @@ module CorrespondenceMarkup
       @parser = CorrespondenceMarkupLanguageParser.new
     end
     
+    def test_template(base_filename)
+      template_filename = File.join(File.dirname(__FILE__), "templates", base_filename)
+      File.read(template_filename)
+    end
+
+    def test_output_from_file(base_filename)
+      output_filename = File.join(File.dirname(__FILE__), "output", base_filename)
+      File.read(output_filename)
+    end
+
     def output_html(template_binding, template_text)
       template = ERB.new(template_text, nil, nil)
       template.result(template_binding)
     end
 
     it "outputs HTML for an item" do
-      item_template_filename = File.join(File.dirname(__FILE__), "templates", "item.html.erb")
-      item_template = File.read(item_template_filename)
+      item_template = test_template("item.html.erb")
       item = Item.new(21, "the text")
-      output_html(binding, item_template).should == %{<span data-id="21">the text</span>}
+      output_html(binding, item_template).should == test_output_from_file("item.output.html")
     end
     
   end
